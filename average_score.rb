2c6e8e3c3573b450f31dde464d24dcd08e3e7f07
eldenhowes <williameldenhowes@gmail.com>
2019-06-06 19:55:34 +0100

    More refactor - remove puts in method

--- average_score.rb
+++ average_score.rb
@@ -4,7 +4,6 @@ def average(scores)
   scores.each do |score|
     scores_accumulator += score
   end
-  puts (scores_accumulator.to_f / scores.length).to_s
   scores_accumulator.to_f / scores.length
 end
 
@@ -12,9 +11,9 @@ test_scores_for_class_1 = [55, 78, 67, 92]
 test_scores_for_class_2 = [48, 99, 91, 70]
 test_scores_for_class_3 = [56, 58, 61, 98, 100]
 
-average(test_scores_for_class_1)
-average(test_scores_for_class_2)
-average(test_scores_for_class_3)
+puts average(test_scores_for_class_1)
+puts average(test_scores_for_class_2)
+puts average(test_scores_for_class_3)
 
-average([average(test_scores_for_class_1),
+puts average([average(test_scores_for_class_1),
   average(test_scores_for_class_2), average(test_scores_for_class_3)]).to_s
